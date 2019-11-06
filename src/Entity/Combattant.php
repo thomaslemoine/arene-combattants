5ac664237c4092ecf10161000f4df2178a5ffd98
Thomas Lemoine <thomas.lemoine@ultro.fr>
2019-11-06 12:08:55 +0100

    fix crud

--- src/Entity/Combattant.php
+++ src/Entity/Combattant.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CombattantRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Combattant
 {
@@ -35,6 +36,37 @@ class Combattant
      * @ORM\Column(type="integer")
      */
     private $pv;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $kill_at;
+
+    /**
+     * @ORM\PrePersist()
+     */
+    public function onPrePersist()
+    {
+        $this->created_at = new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function onPreUpdate()
+    {
+        $this->updated_at = new \DateTime();
+    }
 
     public function getId(): ?int
     {
@@ -85,6 +117,42 @@ class Combattant
     public function setPv(int $pv): self
     {
         $this->pv = $pv;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getKillAt(): ?\DateTimeInterface
+    {
+        return $this->kill_at;
+    }
+
+    public function setKillAt(?\DateTimeInterface $kill_at): self
+    {
+        $this->kill_at = $kill_at;
 
         return $this;
     }
--- src/Migrations/Version20191106103554.php
+++ src/Migrations/Version20191106103554.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191106082127 extends AbstractMigration
+final class Version20191106103554 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,7 @@ final class Version20191106082127 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE combattant (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, strength INT NOT NULL, intelligence INT NOT NULL, pv INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('DROP TABLE combattant_controller');
+        $this->addSql('ALTER TABLE combattant CHANGE updated_at updated_at DATETIME DEFAULT NULL, CHANGE kill_at kill_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -31,7 +30,6 @@ final class Version20191106082127 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE combattant_controller (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, strength INT NOT NULL, intelligence INT NOT NULL, pv INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('DROP TABLE combattant');
+        $this->addSql('ALTER TABLE combattant CHANGE updated_at updated_at DATETIME DEFAULT \'NULL\', CHANGE kill_at kill_at DATETIME DEFAULT \'NULL\'');
     }
 }
